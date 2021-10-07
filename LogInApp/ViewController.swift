d53d2926f736731bef7941622cbc0a70b47334b9
Viktor Shevtsov <v.a.shevtsov@yandex.ru>
2021-10-07 09:38:00 +0300

    added norm alert functional

---
+++ LogInApp/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  LogInApp
+//
+//  Created by Виктор Шевцов on 30.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var userNameTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    var userName = "Murka"
+    var password = "123"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    @IBAction func logInButton(_ sender: UIButton) {
+        if userNameTF.text != userName || passwordTF.text != password {
+            alert (title: "So sorry", message: "Wrong login or password")
+        } else {
+            performSegue(withIdentifier: "settingsSegue", sender: nil)
+        }
+    }
+    
+    @IBAction func forgotUserNameButton(_ sender: UIButton) {
+       alert(title: "Forgot your name?", message: "Your name is \(userName)")
+    }
+    
+    @IBAction func forgotPasswordButton(_ sender: UIButton) {
+      alert(title: "Forgot your password?", message: "Your password is \(password)")
+    }
+    
+    func alert (title:String?,message: String?) {
+        let alert = UIAlertController(title: title,message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("Try once more", comment: "Default action"), style: .default, handler: { _ in
+        NSLog("The \"OK\" alert occured.")
+        }))
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let helloScreenVC = segue.destination as? HelloScreenViewController else {return}
+        helloScreenVC.helloLabel = userNameTF.text
+    }
+    
+    
+}
+
