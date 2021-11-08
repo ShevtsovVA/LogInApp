6050ce4e61e681c880e3e699cfe9c017745fdd49
Viktor Shevtsov <v.a.shevtsov@yandex.ru>
2021-11-08 12:56:43 +0300

    MVC

---
+++ LogInApp/ViewControllers/HelloScreenViewController.swift
@@ -0,0 +1,41 @@
+//
+//  HelloScreenViewController.swift
+//  LogInApp
+//
+//  Created by Виктор Шевцов on 30.09.2021.
+//
+
+import UIKit
+
+class HelloScreenViewController: UIViewController {
+  
+    @IBOutlet weak var helloWordLabel: UILabel!
+    
+    var helloLabel: String!
+    
+    let firstColor = UIColor (red: 210/255, green: 109/255, blue: 128/255, alpha: 1)
+    let secondColor = UIColor (red: 107/255, green: 148/255, blue: 230/255, alpha: 1)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let backGrounGradient = CAGradientLayer ()
+        backGrounGradient.frame = self.view.bounds
+        backGrounGradient.colors = [
+            UIColor.systemCyan.cgColor,
+            UIColor.systemYellow.cgColor
+        ]
+        backGrounGradient.startPoint = CGPoint(x:0,y:0)
+        backGrounGradient.endPoint = CGPoint(x:1,y:1)
+        self.view.layer.addSublayer(backGrounGradient)
+        
+//        (topColor: firstColor, bottomColor: secondColor)
+        
+        helloWordLabel.text = "Hello, \(helloLabel)"
+       
+    
+    }
+    
+
+
+}
--- LogInApp/ViewControllers/ViewController.swift
+++ LogInApp/ViewControllers/ViewController.swift
@@ -12,15 +12,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var userNameTF: UITextField!
     @IBOutlet weak var passwordTF: UITextField!
     
-    var userName = "Murka"
-    var password = "123"
+    private let userName = "Murka"
+    private let password = "123"
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
-    
-    
     @IBAction func logInButton(_ sender: UIButton) {
         if userNameTF.text != userName || passwordTF.text != password {
             alert (title: "So sorry", message: "Wrong login or password")
@@ -37,18 +31,18 @@ class ViewController: UIViewController {
       alert(title: "Forgot your password?", message: "Your password is \(password)")
     }
     
-    func alert (title:String?,message: String?) {
-        let alert = UIAlertController(title: title,message: message, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: NSLocalizedString("Try once more", comment: "Default action"), style: .default, handler: { _ in
+    func alert (title:String,message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("Ok", comment: "Default action"), style: .default, handler: { _ in
         NSLog("The \"OK\" alert occured.")
         }))
         self.present(alert, animated: true, completion: nil)
-        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let helloScreenVC = segue.destination as? HelloScreenViewController else {return}
-        helloScreenVC.helloLabel = userNameTF.text
+        helloScreenVC.helloLabel = userName
+        //userNameTF.text
     }
     
     
